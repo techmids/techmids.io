2a5248fa2c68095495f550e77507e485a3eeb6a7
Luke Browne <luke@thousand.events>
2022-05-31 14:37:58 +0100

    chore: add husky and prettier for code formatting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,22 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Thousand Community</title>
-        <meta
-          name="description"
-          content="Thousand Community believe Birmingham can be the best place to work in tech"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <h1 className="text-2xl">Thousand Community</h1>
-      </main>
-    </>
-  )
-}
+	return (
+		<>
+			<Head>
+				<title>Thousand Community</title>
+				<meta
+					name="description"
+					content="Thousand Community believe Birmingham can be the best place to work in tech"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main>
+				<h1 className="text-2xl">Thousand Community</h1>
+			</main>
+		</>
+	);
+};
 
-export default Home
+export default Home;
