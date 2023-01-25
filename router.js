9da8bca5b65d595f10f456cb5c6c99e4aed12a6d
Alistair Hey <alistair@heyal.co.uk>
2023-01-25 09:41:10 +0000

    Add a team page
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ export const navLinks = [
 	{name: "About", link: "/about"},
 	{name: "Meetups", link: "/meetups"},
 	{name: "Blog", link: "/blog"},
+	{name: "Team", link: "/team"},
 	{name: "CoC", link: "/code-of-conduct"},
 
 ]
